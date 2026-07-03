662f8d236741165d3fecde2326bffd5dc89c4954
Marcus Runge <marcus.runge@outlook.com>
2026-07-03 12:21:37 +0200

    chore: update test SDK and include localization project
    - Update Microsoft.NET.Test.Sdk from 18.6.0 to 18.7.0 in test projects
    - Add MarcusRunge.Toolbox.Localization project to solution

---
+++ MarcusRunge.Toolbox.Localization/EnumDescriptionProvider.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace MarcusRunge.Toolbox.Localization
+{
+    /// <summary>
+    /// Provides localized descriptions for enum values based on the DescriptionAttribute.
+    /// </summary>
+    public static class EnumDescriptionProvider
+    {
+        /// <summary>
+        /// Gets the description of the specified enumeration value.
+        /// </summary>
+        /// <param name="value">The enumeration value.</param>
+        /// <returns>
+        /// The description from <see cref="DescriptionAttribute"/> if available;
+        /// otherwise, the enum value name.
+        /// </returns>
+        public static string GetDescription(Enum value)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+
+            var enumType = value.GetType();
+            var name = Enum.GetName(enumType, value);
+
+            if (name is null)
+                return value.ToString();
+
+            var fieldInfo = enumType.GetField(name);
+
+            if (fieldInfo is null)
+                return value.ToString();
+
+            var attribute = fieldInfo.GetCustomAttributes<DescriptionAttribute>(false).FirstOrDefault();
+
+            return attribute?.Description is string description && !string.IsNullOrWhiteSpace(description) ? description : value.ToString();
+        }
+    }
+}
---
+++ MarcusRunge.Toolbox.Localization/EnumDescriptionValueConverter.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace MarcusRunge.Toolbox.Localization
+{
+    /// <summary>
+    /// Converts enum values to their localized description if a DescriptionAttribute is present.
+    /// </summary>
+    /// <seealso cref="System.Windows.Data.IValueConverter" />
+    public sealed class EnumDescriptionValueConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converts a value.
+        /// </summary>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>
+        /// A converted value. If the method returns <see langword="null" />, the valid null value is used.
+        /// </returns>
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) => value is Enum enumValue ? EnumDescriptionProvider.GetDescription(enumValue) : value;
+
+        /// <summary>
+        /// Converts a value.
+        /// </summary>
+        /// <param name="value">The value that is produced by the binding target.</param>
+        /// <param name="targetType">The type to convert to.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>
+        /// A converted value. If the method returns <see langword="null" />, the valid null value is used.
+        /// </returns>
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) => Binding.DoNothing;
+    }
+}
